986178dba8b0145d835d9e5ba04350e65d44d346
Hadley King <hadley_king@gwu.edu>
2021-12-13 17:57:20 -0500

    PRJNA231221_AssemblyUpdated with taxonomy info Changes to be committed: 	new file:   data_files/PRJNA231221_AssemblyUpdated.csv 	modified:   lib/linage.py

--- lib/linage.py
+++ lib/linage.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
+import csv, time
 from Bio import Entrez
-
+csv_file = 'data_files/PRJNA231221_AssemblyDetails.csv'
+Entrez.email = "hadley_king@gwu.edu"
 bioproject = {}
 
-with open('test.csv', 'r') as file:
+with open(csv_file, 'r') as file:
     data = csv.reader(file)
     header = next(data)
     for row in data: 
         bioproject[row[0]] = row[1:]
-print(bioproject)
-test_id = bioproject['GCA_013267415.1'][5]
-print(test_id)
+
+header.append('Taxonomy ID')
+header.append('Full Lineage')
+with open('csv_file.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(header)
+    for key in bioproject:
+        handle = Entrez.esearch(db="taxonomy", term=bioproject[key][5])
+        record = Entrez.read(handle)
+        handle.close()
+        print(record)
+        try: 
+            bioproject[key].append(record['IdList'][0])
+            print(record['IdList'])
+            handle2 = Entrez.efetch(db='taxonomy', id=record['IdList'][0])
+            record2 = Entrez.read(handle2)
+            print(record2[0]['Lineage'])
+            bioproject[key].append(record2[0]['Lineage'])
+        except: 
+            bioproject[key].append('')
+            bioproject[key].append('')
+        time.sleep(1)
+        row = [key]
+        for item in bioproject[key]:
+            row.append(item)
+        writer.writerow(row)
 # print(",".join(assembly_ids))
-Entrez.email = "hadley_king@gwu.edu"
 # print(Entrez.epost("assembly", id=",".join(assembly_ids)).read())
-handle = Entrez.esearch(db="taxonomy", term=test_id)
-record = Entrez.read(handle)
-handle.close()
-print(record['IdList'])
-handle2 = Entrez.efetch(db='taxonomy', id=record['IdList'][0])
-record2 = Entrez.read(handle2)
-print(record2[0]['Lineage'])
+

82c6d4c94ecc71f03340e65b4c8271957c1b954d
Hadley King <hadley_king@gwu.edu>
2022-04-11 18:08:51 -0400

    Updated converter for datetime and urls Changes to be committed: 	modified:   lib/galaxy_converter.py

--- lib/galaxy_converter.py
+++ lib/galaxy_converter.py
@@ -5,10 +5,11 @@
 Used to convert all Galaxy exported BCO BEFORE Galaxy version 22.05
 """
 
-from encodings import utf_8
 import json
 import argparse
 import sys
+from dateutil.parser import parse, ParserError
+from urllib.parse import urlparse
 
 __version__ = "1.0.0"
 __status__ = "Production"
@@ -47,6 +48,47 @@ def usr_args():
     options = parser.parse_args()
     return options
 
+def timezone_aware(date_text):
+    """Makes a datetime Timezone aware
+
+    Parameters
+    ----------
+    date_text: str
+        Datetime object as a string.
+
+    Returns
+    -------
+    timezone_text: str
+        Datetime object as a string with timezone info.
+    """
+    date_obj = parse(date_text)
+    if date_obj.tzinfo is None:
+        # print('converting')
+        date_t = date_obj.strftime("%Y-%m-%dT%H:%M:%S-04:00")
+        return date_t
+    # print('no conversion needed')
+    return date_text
+
+def url_valid(url):
+    """Validate a URL
+
+    Parameters
+    ----------
+    url: str
+        String as a URL to validate
+    Returns
+    -------
+    bool
+        A True or False value for the URL supplied
+
+    """
+
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
 def convert_bco(bco):
     """Convert BCO
 
@@ -61,29 +103,38 @@ def convert_bco(bco):
     Returns
     -------
     bco_dict: dict
-        Converted dictionary object that when output ot JSON will be 
+        Converted dictionary object that when output ot JSON will be
         IEEE-2791-2020 complient
     """
 
-
-    with open(bco, 'r', encoding=utf_8) as file:
+    with open(bco, 'r', encoding='utf_8') as file:
         bco_dict = json.load(file)
+
+    bco_dict['provenance_domain']['created'] = timezone_aware(
+        bco_dict['provenance_domain']['created']
+    )
+    bco_dict['provenance_domain']['modified'] = timezone_aware(
+        bco_dict['provenance_domain']['modified']
+    )
     for key in bco_dict['error_domain']:
         if bco_dict['error_domain'][key] == []:
             bco_dict['error_domain'][key] = {}
     if bco_dict['provenance_domain']['version'] != str():
-        bco_dict['provenance_domain']['version'] = str(bco_dict['provenance_domain']['version'])
-    for item in bco_dict['provenance_domain']['review']:
-        item['reviewer']['contribution'] = list([item['reviewer']['contribution']])
-        del item['reviewer']['orcid']
+        bco_dict['provenance_domain']['version'] = str(
+            bco_dict['provenance_domain']['version']
+        )
+    if bco_dict['provenance_domain']['review']:
+        del bco_dict['provenance_domain']['review']
     for item in bco_dict['provenance_domain']['contributors']:
         del item['orcid']
     for index, item in enumerate(bco_dict['execution_domain']['script']):
         if isinstance(item, dict) is False:
-            bco_dict['execution_domain']['script'][index] = {'uri': {'uri': item}}
+            bco_dict['execution_domain']['script'][index]={'uri':{'uri':item}}
     if bco_dict['execution_domain']['script_access_type']:
         del bco_dict['execution_domain']['script_access_type']
-    for index, item in enumerate(bco_dict['execution_domain']['external_data_endpoints']):
+    for index, item in enumerate(
+        bco_dict['execution_domain']['external_data_endpoints']
+    ):
         if item is None:
             del bco_dict['execution_domain']['external_data_endpoints'][index]
     for index, item in enumerate(bco_dict['parametric_domain']):
@@ -91,6 +142,58 @@ def convert_bco(bco):
             bco_dict['parametric_domain'][index]['step'] = str(item['step'])
         if item['value'] != str():
             bco_dict['parametric_domain'][index]['value'] = str(item['value'])
+    for item in bco_dict['execution_domain']['software_prerequisites']:
+        if not url_valid(item['uri']['uri']):
+            item['uri']['uri'] = 'https://'+item['uri']['uri']
+    for step in bco_dict['description_domain']['pipeline_steps']:
+        for input_obj in step['input_list']:
+            try:
+                input_obj['access_time'] = timezone_aware(
+                    input_obj['access_time']
+                )
+            except ParserError:
+                del input_obj['access_time']
+
+        for output_obj in step['output_list']:
+            try:
+                output_obj['access_time'] = timezone_aware(
+                    output_obj['access_time']
+                )
+            except ParserError:
+                del output_obj['access_time']
+
+        for preq_obj in step['prerequisite']:
+            try:
+                preq_obj['access_time'] = timezone_aware(
+                    preq_obj['access_time']
+                )
+            except ParserError:
+                del preq_obj['access_time']
+
+    for prereq in bco_dict['execution_domain']['software_prerequisites']:
+        try:
+            prereq['uri']['access_time'] = timezone_aware(
+                prereq['uri']['access_time']
+            )
+        except ParserError:
+            del prereq['uri']['access_time']
+
+    for input_obj in bco_dict['io_domain']['input_subdomain']:
+        try:
+            input_obj['uri']['access_time'] = timezone_aware(
+                input_obj['uri']['access_time']
+            )
+        except ParserError:
+            del input_obj['uri']['access_time']
+
+    for output_obj in bco_dict['io_domain']['output_subdomain']:
+        try:
+            output_obj['uri']['access_time'] = timezone_aware(
+                output_obj['uri']['access_time']
+            )
+
+        except ParserError:
+            del output_obj['uri']['access_time']
 
     return bco_dict
 

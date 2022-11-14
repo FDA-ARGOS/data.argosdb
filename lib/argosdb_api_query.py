563bf840814e2231e090fadf3dfb51c577ca87cf
Hadley King <HadleyKing@users.noreply.github.com>
2022-11-14 13:06:11 -0500

    updating (#119)
    * fix broken file names
    
    * Added ngs_id_script.py and template docs
    
    * Added ngs_id_script.py and template dataset
    
    * Bump numpy from 1.21.4 to 1.22.0
    
    Bumps [numpy](https://github.com/numpy/numpy) from 1.21.4 to 1.22.0.
    - [Release notes](https://github.com/numpy/numpy/releases)
    - [Changelog](https://github.com/numpy/numpy/blob/main/doc/RELEASE_WALKTHROUGH.rst)
    - [Commits](https://github.com/numpy/numpy/compare/v1.21.4...v1.22.0)
    
    ---
    updated-dependencies:
    - dependency-name: numpy
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * ngs from API (#115)
    
    * fix broken file names
    
    * Added ngs_id_script.py and template docs
    
    * Added ngs_id_script.py and template dataset
    
    * message Please enter the commit message for your changes. Lines starting
    
    * updated script
    
    Co-authored-by: hadleyking <hadley_king@gwu.edu>
    
    * Update galaxy_converter.py
    
    * ARGOSDB API argosdb_api_query
    
    Changes to be committed:
    	new file:   lib/argosdb_api_query.py
    	modified:   lib/assembly_qc_ncbi_.py
    
    * saving changes
    
    * make argosdb_api_query.py script
    
    Fix #95
    Changes to be committed:
    	modified:   lib/assembly_qc_ncbi_.py
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: jgergely11 <jgergely11@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Jgergely11 <96205065+Jgergely11@users.noreply.github.com>

---
+++ lib/argosdb_api_query.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Argos DB API Query
+
+Entry point for ARGOS DB
+"""
+
+__version__ = "0.1.0"
+__status__ = "BETA"
+
+import os
+import requests
+api_url = "https://api.argosdb.org/records/search"
+results = []
+assemblies = []
+data = [{
+  "bcoid": "ARGOS_000012",
+  "offset": 1,
+  "limit": 10000
+},
+{
+  "bcoid": "ARGOS_000022",
+  "offset": 1,
+  "limit": 10000
+}]
+os.system('mkdir home/assembly')
+for item in data:
+    response = requests.post(api_url, json=item)
+    results.append(response.json())
+    for record in response.json()['recordlist']:
+        if record['genome_assembly_id'] not in assemblies:
+            assemblies.append(record['genome_assembly_id'])
+            os.system(f"efetch -db assembly -id {record['genome_assembly_id']} -format docsum > home/{record['genome_assembly_id']}.xml")
+    print(response.status_code)
+print(assemblies)

cc5e1170527b69f0bcddaa170004cf9bd6e04520
hadleyking <hadley_king@gwu.edu>
2023-01-28 09:46:09 -0500

    ngsQC_NCBI.tsv
    Fix #144
    	new file:   lib/add_lineage.py
    	modified:   lib/efetch_bioproject_runs.sh
    	modified:   lib/eutils_SRAcurl_experiments.sh
    	new file:   lib/get_linage.py
    	deleted:    lib/linage.py
    	new file:   lib/sra_run_info_parser.py

--- lib/eutils_SRAcurl_experiments.sh
+++ lib/eutils_SRAcurl_experiments.sh
@@ -2,7 +2,7 @@
 
 # Curl for NCBI Trace to retrieve SRR analysis values
 # Need to provide a text file with a list of SRR accessions
-for i in $(cat home/PRJNA231221_runs.txt )
+for i in $(cat home/PRJEB12890_runs.txt )
 do
 	 var1='https://trace.ncbi.nlm.nih.gov/Traces/sra/?run='
 	 var2=$i'&retmode=xml'

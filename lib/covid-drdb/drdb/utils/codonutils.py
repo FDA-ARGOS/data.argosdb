5991385fcfbb12bd98baddd826614269af950f65
Jingyue Wu <61590108+JingyueWu@users.noreply.github.com>
2023-03-22 12:23:51 -0400

    Create codonutils.py

---
+++ lib/covid-drdb/drdb/utils/codonutils.py
@@ -0,0 +1,149 @@
+from typing import Dict, List, Set
+
+
+AMINO_ACID_LIST: List[int] = [
+    ord(b'A'),
+    ord(b'C'),
+    ord(b'D'),
+    ord(b'E'),
+    ord(b'F'),
+    ord(b'G'),
+    ord(b'H'),
+    ord(b'I'),
+    ord(b'K'),
+    ord(b'L'),
+    ord(b'M'),
+    ord(b'N'),
+    ord(b'P'),
+    ord(b'Q'),
+    ord(b'R'),
+    ord(b'S'),
+    ord(b'T'),
+    ord(b'V'),
+    ord(b'W'),
+    ord(b'Y'),
+    ord(b'*')
+]
+
+
+CODON_TABLE: Dict[bytes, bytes] = {
+    b'TTT': b'F',
+    b'TTC': b'F',
+    b'TTA': b'L',
+    b'TTG': b'L',
+
+    b'CTT': b'L',
+    b'CTC': b'L',
+    b'CTA': b'L',
+    b'CTG': b'L',
+
+    b'ATT': b'I',
+    b'ATC': b'I',
+    b'ATA': b'I',
+    b'ATG': b'M',
+
+    b'GTT': b'V',
+    b'GTC': b'V',
+    b'GTA': b'V',
+    b'GTG': b'V',
+
+    b'TCT': b'S',
+    b'TCC': b'S',
+    b'TCA': b'S',
+    b'TCG': b'S',
+
+    b'CCT': b'P',
+    b'CCC': b'P',
+    b'CCA': b'P',
+    b'CCG': b'P',
+
+    b'ACT': b'T',
+    b'ACC': b'T',
+    b'ACA': b'T',
+    b'ACG': b'T',
+
+    b'GCT': b'A',
+    b'GCC': b'A',
+    b'GCA': b'A',
+    b'GCG': b'A',
+
+    b'TAT': b'Y',
+    b'TAC': b'Y',
+
+    b'CAT': b'H',
+    b'CAC': b'H',
+    b'CAA': b'Q',
+    b'CAG': b'Q',
+
+    b'AAT': b'N',
+    b'AAC': b'N',
+    b'AAA': b'K',
+    b'AAG': b'K',
+
+    b'GAT': b'D',
+    b'GAC': b'D',
+    b'GAA': b'E',
+    b'GAG': b'E',
+
+    b'TGT': b'C',
+    b'TGC': b'C',
+    b'TGG': b'W',
+
+    b'CGT': b'R',
+    b'CGC': b'R',
+    b'CGA': b'R',
+    b'CGG': b'R',
+
+    b'AGT': b'S',
+    b'AGC': b'S',
+    b'AGA': b'R',
+    b'AGG': b'R',
+
+    b'GGT': b'G',
+    b'GGC': b'G',
+    b'GGA': b'G',
+    b'GGG': b'G',
+
+    b'TAA': b'*',
+    b'TGA': b'*',
+    b'TAG': b'*',
+}
+
+REVERSE_CODON_TABLE: Dict[int, List[bytes]] = {}
+for codon, aa in CODON_TABLE.items():
+    REVERSE_CODON_TABLE.setdefault(aa[0], []).append(codon)
+
+
+AMBIGUOUS_NAS: Dict[int, bytes] = {
+    ord(b'W'): b'AT',
+    ord(b'S'): b'CG',
+    ord(b'M'): b'AC',
+    ord(b'K'): b'GT',
+    ord(b'R'): b'AG',
+    ord(b'Y'): b'CT',
+    ord(b'B'): b'CGT',
+    ord(b'D'): b'AGT',
+    ord(b'H'): b'ACT',
+    ord(b'V'): b'ACG',
+    ord(b'N'): b'ACGT'
+}
+
+
+def expand_ambiguous_na(na: int) -> bytes:
+    return AMBIGUOUS_NAS.get(na, bytes([na]))
+
+
+def translate_codon(nas: bytes) -> bytes:
+    aas_text: bytes
+    nas = nas.replace(b'-', b'N')[:3]
+    if nas in CODON_TABLE:
+        return CODON_TABLE[nas]
+    aas: Set[int] = set()
+    for na0 in AMBIGUOUS_NAS.get(nas[0], nas[0:1]):
+        for na1 in AMBIGUOUS_NAS.get(nas[1], nas[1:2]):
+            for na2 in AMBIGUOUS_NAS.get(nas[2], nas[2:3]):
+                aas |= set(
+                    CODON_TABLE[bytes([na0, na1, na2])]
+                )
+    CODON_TABLE[nas] = aas_text = bytes((sorted(aas)))
+    return aas_text

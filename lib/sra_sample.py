abfe918bdffa7ddd2d0d18d848fc0f3b0e6a17ac
Jgergely11 <96205065+Jgergely11@users.noreply.github.com>
2023-08-08 10:37:29 -0400

    Update sra_sample.py

--- lib/sra_sample.py
+++ lib/sra_sample.py
@@ -11,8 +11,11 @@ import sys
 import csv
 import argparse
 import xml.etree.ElementTree as ET
-__version__ = "0.1.0"
+
+__version__ = "0.1.1"
 __status__ = "Beta"
+
+PLACEHOLDER_CHAR = ''
 
 def usr_args():
     """Program Arguments
@@ -38,6 +41,10 @@ def usr_args():
         required=True,
         help="Input file. The input file should be a collection of SRS XML \
             files.")
+    parser.add_argument('-b', '--bco_id',
+        help="BCO ID")
+    parser.add_argument('-s', '--schema',
+        help="Schema version")
     parser.add_argument('-o', '--output',
         help="Output file. If no output is provided, the resulting table will \
             be output to the terminal.")
@@ -46,7 +53,7 @@ def usr_args():
 
     return parser.parse_args()
 
-def parse_xml( xml_file, samples):
+def parse_xml( xml_file, samples, bco_id, schema_version):
     """Parse XML file
 
     Parameters
@@ -62,12 +69,18 @@ def parse_xml( xml_file, samples):
         dictionary of samples
     """
 
-    biosample_id = sra_id = strain = organism_name = sample_name \
-        = ncbi_taxonomy_id = isolate = collected_by = collection_date \
+    organism_name = infraspecific_name = lineage = taxonomy_id \
+        = bioproject = biosample \
+        = strain = genome_assembly_id = isolate = sample_name \
+        = instrument = collected_by = collection_date \
         = geo_loc_name = isolation_source = lat_lon = culture_collection = host\
-        = host_age = host_description = host_disease = host_disease_outcome = \
-        host_disease_stage = host_health_state = host_sex \
-        = identification_method = type_material = '-'
+        = host_age = host_description = host_disease = host_disease_outcome \
+        = host_disease_stage = host_health_state = host_sex \
+        = id_method = biosample_score = PLACEHOLDER_CHAR
+
+    bco_id = 'ARGOS_000010'
+    bioproject = 'PRJNA231221'
+    schema_version = schema_version or PLACEHOLDER_CHAR
 
     # create element tree object and get root element
     root = ET.parse(xml_file).getroot()
@@ -80,13 +93,16 @@ def parse_xml( xml_file, samples):
                 if 'db_label' in feature.attrib.keys():
                     sample_name = feature.text
                 if feature.tag == 'Organism':
-                    ncbi_taxonomy_id = feature.attrib['taxonomy_id']
+                    taxonomy_id = feature.attrib['taxonomy_id']
                     organism_name = feature.attrib['taxonomy_name']
+         
         for attribute in biosample.findall('./Attributes/'):
             if attribute.attrib['attribute_name'] == 'strain':
                 strain = attribute.text
             if attribute.attrib['attribute_name'] == 'isolate':
-                isolate = attribute.text
+                isolate= attribute.text
+            if attribute.attrib['attribute_name'] == 'sample_name':
+                sample_name = attribute.text
             if attribute.attrib['attribute_name'] == 'collected_by':
                 collected_by = attribute.text
             if attribute.attrib['attribute_name'] == 'collection_date':
@@ -119,13 +135,19 @@ def parse_xml( xml_file, samples):
                 identification_method = attribute.text
             if attribute.attrib['attribute_name'] == 'type-material':
                 type_material = attribute.text
+        
 
-        samples[biosample_id] = [sra_id, organism_name, strain, sample_name, \
-            ncbi_taxonomy_id, isolate, collected_by, collection_date, \
+        samples[biosample_id] = [organism_name, infraspecific_name, lineage, taxonomy_id, bco_id, \
+            schema_version, bioproject, biosample_id, strain, genome_assembly_id, \
+            sample_name, instrument, isolate, collected_by, collection_date, \
             geo_loc_name, isolation_source, lat_lon, culture_collection, host,\
             host_age, host_description, host_disease, host_disease_outcome, \
             host_disease_stage, host_health_state, host_sex, \
-            identification_method, type_material]
+            identification_method]
+
+        biosample_score = len(samples[biosample_id])-samples[biosample_id].count(PLACEHOLDER_CHAR)
+        samples[biosample_id].append(biosample_score)
+
 
     return samples
 
@@ -148,11 +170,12 @@ def sample_output( samples, header, output):
 
     if output:
         sample_file = os.path.abspath(output)
-        with open(sample_file, 'w',  encoding='utf8') as file:
+        with open(sample_file, 'w',  encoding='utf8', newline = '') as file:
             writer = csv.writer(file, header, delimiter='\t')
             writer.writerow(header)
             for key in samples:
-                row = [key]
+                #row = [key] # biosample_id is no longer first in order
+                row = []
                 for item in range(len(samples[key])):
                     row.append(samples[key][item])
                 writer.writerow(row)
@@ -160,21 +183,24 @@ def sample_output( samples, header, output):
     else:
         print('\t'.join(item for item in header))
         for run in samples:
-            print(run, '\t', '\t'.join(str(item) for item in samples[run]))
+            print('\t'.join(str(item) for item in samples[run]))
+    
+
 
 def main():
-    """Main Fuunction
+    """Main Function
     """
     samples = {}
-    header = ['biosample_id', 'sra_id', 'organism_name', 'strain', \
-        'sample_name', 'ncbi_taxonomy_id', 'isolate', 'collected_by',\
+    header = ['organism_name', 'infraspecific_name', 'lineage', 'taxonomy_id', 'bco_id', \
+        'schema_version', 'bioproject', 'biosample', 'strain', 'genome_assembly_id',\
+        'sample_name', 'instrument', 'isolate', 'collected_by',\
         'collection_date', 'geo_loc_name', 'isolation_source', 'lat_lon',\
         'culture_collection', 'host', 'host_age', 'host_description', \
         'host_disease', 'host_disease_outcome', 'host_disease_stage',\
-        'host_health_state', 'host_sex', 'identification_method',\
-        'type_material']
+        'host_health_state', 'host_sex', 'id_method',\
+        'biosample_score']
     args = usr_args()
-    parse_xml(args.file, samples)
+    parse_xml(args.file, samples, args.bco_id, schema_version = args.schema)
     sample_output(samples, header, args.output)
 
 if __name__ == "__main__":
